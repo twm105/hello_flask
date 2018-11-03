05f0cc9487c0ff264348f4819c8312deec3915aa
Tom Maisey <tmaisey@gmail.com>
2018-11-03 22:42:08 +0000

    Create Part 7 of the Flask Mega Tutorial

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 
 app = Flask(__name__)
@@ -13,4 +16,41 @@ login = LoginManager(app)
 login.login_view = 'login' # tells Flask-Login the login view (route) function
 
 
-from app import routes, models
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+        
+        # GMAIL MAIL SERVER DETAILS (allow 'less secure apps'):
+        # set MAIL_SERVER=smtp.googlemail.com
+        # set MAIL_PORT=587
+        # set MAIL_USE_TLS=1
+        # set MAIL_USERNAME=<your-gmail-username>
+        # set MAIL_PASSWORD=<your-gmail-password>
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+    # logging categories: DEBUG, INFO, WARNING, ERROR, CRITICAL
+
+
+from app import routes, models, errors
