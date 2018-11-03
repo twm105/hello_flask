a74af2060eb442daa444a8a95d857ba58ce6772a
Tom Maisey <tmaisey@gmail.com>
2018-11-03 16:32:32 +0000

    Create Part 6 of the Flask Mega Tutorial

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,21 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
 from sqlalchemy import func
+from datetime import datetime
+
+# user_count = db.session.query(func.count(User.id)).scalar()
 
 
-user_count = db.session.query(func.count(User.id)).scalar()
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+        # Flask invokes user loader callback fcn from current_user, so don't need to add to session
 
 
 @app.route('/')
@@ -25,7 +33,7 @@ def index():
         }
     ]
     return render_template(
-        'index.html', title='Home Page', posts=posts, user_count=user_count)
+        'index.html', title='Home Page', user=current_user, posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -44,7 +52,7 @@ def login():
             next_page = url_for('index')
         return redirect(next_page)
     return render_template(
-        'login.html', title='Sign In', form=form, user_count=user_count)
+        'login.html', title='Sign In', form=form)
 
 
 @app.route('/logout')
@@ -66,4 +74,32 @@ def register():
         flash('Welcome, {}. Registration successful.'.format(user.username))
         return redirect(url_for('login'))
     return render_template(
-        'register.html', title='Register', form=form, user_count=user_count)
+        'register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': '1st post body...'},
+        {'author': user, 'body': '2nd post body...'}
+    ]
+    return render_template('user.html', title=user.username, user=user, posts=posts)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit() # current_user callback negates need for db.session.add
+        flash('Profile changes saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', 
+                            form=form)
